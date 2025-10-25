e3ddd88c230212c9fc561cda75a872e9faa747f9
Manonrult <manon-ru@live.fr>
2025-10-25 20:11:37 +0200

    Modification du Layout + Projets.json

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useLayoutEffect } from 'react'; //
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 
 function Layout() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
-    <div class="layout-container">
+    <div className="layout-container">
       <Header />
+
       <main className="content">
         <Outlet />
       </main>
